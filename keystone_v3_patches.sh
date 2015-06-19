8545b20284eeb0f225d752726b366185b576db59
Ivan Chavero <ichavero@redhat.com>
2015-06-19 17:01:48 -0600

    Add script for patching the keystone v3 module
    Applies V3 API patches for the keystone puppet module

---
+++ keystone_v3_patches.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cd /usr/share/openstack-puppet/
+echo "Cleaning up before patching..."
+rm -rf puppet-keystone.git
+git clone https://github.com/stackforge/puppet-keystone.git puppet-keystone.git
+cd puppet-keystone.git
+echo "Applying support for keystone v3 api - v3 domain utility code"
+# support for keystone v3 api - v3 domain utility code
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/85/178385/20 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - the domain resource"
+# support for keystone v3 api - the domain resource
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/39/174439/32 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - the project resource"
+# support for keystone v3 api - the project resource
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/68/174468/29 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - the user resource"
+# support for keystone v3 api - the user resource
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/76/174976/25 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - the user_role resource"
+# support for keystone v3 api - the user_role resource
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/99/174999/25 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - keystone and keystone::roles::admin"
+# support for keystone v3 api - keystone and keystone::roles::admin
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/60/175060/30 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - make_keystone_user_role_name"
+# support for keystone v3 api - make_keystone_user_role_name
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/55/193355/1 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - keystone::resource::authconfig"
+# support for keystone v3 api - keystone::resource::authconfig
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/56/193356/1 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - resource::service_identity"
+# support for keystone v3 api - resource::service_identity
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/57/193357/1 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - update keystone::roles::admin"
+# support for keystone v3 api - update keystone::roles::admin
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/58/193358/1 && git cherry-pick FETCH_HEAD
+echo "Applying support for keystone v3 api - keystone::endpoint"
+# support for keystone v3 api - keystone::endpoint
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/59/193359/1 && git cherry-pick FETCH_HEAD
+
+echo "Patching finished"
+
+echo "Creating symlink"
+cd ../modules
+rm -rf keystone
+ln -s ../puppet-keystone.git keystone
+
+
