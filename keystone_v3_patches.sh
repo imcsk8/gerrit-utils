027640c24e9b732dfffa7286038c79394434f0d7
Ivan Chavero <ichavero@redhat.com>
2015-06-29 21:50:29 -0600

    Add latest patches

--- keystone_v3_patches.sh
+++ keystone_v3_patches.sh
@@ -7,22 +7,22 @@ git clone https://github.com/stackforge/puppet-keystone.git puppet-keystone.git
 cd puppet-keystone.git
 echo "Applying support for keystone v3 api - v3 domain utility code"
 # support for keystone v3 api - v3 domain utility code
-git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/85/178385/20 && git cherry-pick FETCH_HEAD
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/85/178385/23 && git cherry-pick FETCH_HEAD
 echo "Applying support for keystone v3 api - the domain resource"
 # support for keystone v3 api - the domain resource
-git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/39/174439/32 && git cherry-pick FETCH_HEAD
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/39/174439/35 && git cherry-pick FETCH_HEAD
 echo "Applying support for keystone v3 api - the project resource"
 # support for keystone v3 api - the project resource
-git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/68/174468/29 && git cherry-pick FETCH_HEAD
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/68/174468/33 && git cherry-pick FETCH_HEAD
 echo "Applying support for keystone v3 api - the user resource"
 # support for keystone v3 api - the user resource
-git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/76/174976/25 && git cherry-pick FETCH_HEAD
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/76/174976/29 && git cherry-pick FETCH_HEAD
 echo "Applying support for keystone v3 api - the user_role resource"
 # support for keystone v3 api - the user_role resource
-git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/99/174999/25 && git cherry-pick FETCH_HEAD
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/99/174999/29 && git cherry-pick FETCH_HEAD
 echo "Applying support for keystone v3 api - keystone and keystone::roles::admin"
 # support for keystone v3 api - keystone and keystone::roles::admin
-git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/60/175060/30 && git cherry-pick FETCH_HEAD
+git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/60/175060/34 && git cherry-pick FETCH_HEAD
 echo "Applying support for keystone v3 api - make_keystone_user_role_name"
 # support for keystone v3 api - make_keystone_user_role_name
 git fetch https://ichavero@review.openstack.org/openstack/puppet-keystone refs/changes/55/193355/1 && git cherry-pick FETCH_HEAD
